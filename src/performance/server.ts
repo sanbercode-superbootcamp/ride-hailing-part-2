8984030d389c11d60760b873976328c063ccfa0a
Hasan-Nabil <crispy.crunchy.creamy.chips@gmail.com>
2019-10-22 08:44:14 +0700

    challenge ridehailing p2 Hasan-Nabil

--- src/performance/server.ts
+++ src/performance/server.ts
@@ -4,15 +4,20 @@ import { createServer } from 'http';
 import { Server } from 'net';
 import { json as jsonBodyParser } from 'body-parser';
 import { riderPerformance } from './performance';
+import * as swaggerUI from 'swagger-ui-express';
+
+const swaggerSpec = require('../../swagger/performance.json');
 
 const PORT = process.env['RH_PORT'] || 3003;
 
 const app = express();
 app.set('port', PORT);
 app.use(cors());
+app.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerSpec));
 
 app.get('/point/:rider_id', jsonBodyParser(), riderPerformance);
 app.all("*", (req, res) => res.status(404).send());
+
 const server = createServer(app);
 
 export function startServer(): Server {
